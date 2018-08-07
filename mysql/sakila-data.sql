12dd424e26f5309368ac3d0783f4cbfd519011a7
508821881@qq.com <508821881@qq.com>
2018-08-07 11:43:57 +0800

    [ADD]docker-mysql

--- mysql/sakila-data.sql
+++ mysql/sakila-data.sql
@@ -1,3 +1,3 @@
-INSERT INTO userinfo(id,username) VALUES(0,'打哈吉');
+INSERT INTO userinfo(id,username) VALUES(0,'CSDN yunlingfly');
 INSERT INTO userinfo(id,username) VALUES(1,'dakl');
 INSERT INTO userinfo(id,username) VALUES(2,'dhasjk');
